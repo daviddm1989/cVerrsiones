af97fe1d3507b40a9f58685c6c1dccbf79047ccb
JAIME-PC <jaime33m95@gmail.com>
2017-03-11 18:47:35 +0100

    Coreccción de errores de la clase Cliente y añadidos comentarios

--- Almacen/src/almacen/Cliente.java
+++ Almacen/src/almacen/Cliente.java
@@ -6,15 +6,19 @@
 package almacen;
 
 /**
- *
+ *	Clase cliente que almacenara toda la informacion relacionada con el cliente
  * @author david
  */
 public class Cliente {
-      private int id;
+    private int id;
     private String nombre;
     private String direccion;
     private int numArticulosComprados;
     
+    /**
+     * Constructor de la clase cliente
+     * @param i, para generar la ID de forma ascendete @see Almacen donde crear cliente
+     */
 public Cliente(int i){
     this.nombre=generarNombre();
     this.id=generarId(i);
@@ -23,8 +27,11 @@ public Cliente(int i){
 }
 
    
-
-    public String generarNombre(){
+	/**
+	 * Metodo que se encarga de generar un nombre aleatorio dependiendo de i (numero aleatorio)
+	 * @return el nombre obtenido para asignarlo a su cliente
+	 */
+    private String generarNombre(){
         int i=(int) (Math.random()*5+1);
         switch(i){
             case 1:nombre="Maria";
@@ -44,7 +51,12 @@ public Cliente(int i){
         }
         return nombre;
     }
-    public String generarDireccion(){
+    
+    /**
+     * Metodo que se encarga de generar una direccion aleatorio dependiendo de i (numero aleatorio)
+	 * @return la direccion obtenido para asignarlo a su cliente
+     */
+    private String generarDireccion(){
         int i=(int) (Math.random()*5+1);
         switch(i){
             case 1:nombre="C/San Gregorio Nº 98";
@@ -64,29 +76,61 @@ public Cliente(int i){
         }
         return nombre;
     }
-    public int generarId(int i){
+    
+    /**
+     * Metodo que genera la id del cliente de forma ascendete
+     * @param i, @see Almacen como crear cliente
+     * @return la id correspondiente al cliente
+     */
+    private int generarId(int i){
         this.id=i+1;
         return id;
     }
     
+    /**
+     * Metodo de modificacion de la id del cliente
+     * @param id del cliente actual
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Metodo de modificacion del nombre del cliente
+     * @param nombre del cliente actual
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-     public void setDireccion(){
+    
+    /**
+     * Metodo de modificacion de la direccion actual del cliente
+     * @param direccion del cliente actual
+     */
+     public void setDireccion(String direccion){
         this.direccion=direccion;
     }
 
+     /**
+      * Metodo de consulta del atributo id del cliente
+      * @return la id perteneciente al cliente
+      */
     public int getId() {
         return id;
     }
 
+    /**
+     * Metodo de consulta del atributo nombre del cliente
+     * @return el nombre que pertenece al cliente
+     */
     public String getNombre() {
         return nombre;
     }
+    
+    /**
+     * Metodo de consulta del atributo direccion del cliente
+     * @return la direccion que posee el cliente
+     */
     public String getDireccion() {
         return direccion;
     }
